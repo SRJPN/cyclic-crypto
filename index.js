dce4f289a918a2c15a3deb7c0a103fe5220aae5a
Sooraj Parameswaran <soorajp@thoughtworks.com>
2019-11-20 22:12:12 +0530

    Got a working solution

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-// (x1, y1) + (x2, y2) = 1/r (x1.x2 - y1.y2), (y1.x2 +x1.y2) Mod 11
-// 
-// y*y % 11 = 9-x*x % 11
+const modInverse = (a, m) => {
+  a = a % m;
+  for (x = 1; x < m; x++) {
+    if ((a * x) % m == 1)
+      return x;
+  }
+  return 1;
+}
 
-// x1 = 7
-// y1 = 2
-// x2 = 9
-// y2 = 4
-
-// // [9,4]
-
-// X1 = (x1 * x2 - y1 * y2) % 11;
-// Y1 = (y1 * x2) + (x1 * y2) % 11
+const split = (s) => {
+  const median = Math.ceil(s.length / 2);
+  return [s.substr(0, median), s.substr(median, s.length)];
+}
 
 const format = (list) => list.map(([x, y]) => `${x}${y}`).join();
 
-const modeEleven = n => {
-  const p = 11;
+const mode = (n, m) => {
   if (n < 0)
-    n = p - Math.abs(n) % p;
+    n = m - Math.abs(n) % m;
 
-  return n % p;
+  return n % m;
 };
+
+const modeEleven = n => mode(n, 11);
 
 const isCElevenThreeGroup = (x, y) => {
   return modeEleven(y * y) === modeEleven(9 - (x * x));
@@ -31,35 +32,24 @@ const xFunction = ([x1, y1], [x2, y2]) => {
   const y1y2 = y1 * y2;
   const y1x2 = y1 * x2;
   const x1y2 = x1 * y2;
-  if (isCElevenThreeGroup(x1, y1)) {
-    const X = x1x2 - y1y2
-    const Y = modeEleven(y1x2 + x1y2);
-    return [
-      (modeEleven(X) / 3),
-      (Y / 3)
-    ];
-  }
+
   const X = x1x2 - y1y2
   const Y = y1x2 + x1y2;
   return [
-    modeEleven(X / 3),
-    modeEleven(Y / 3)
+    modeEleven(modeEleven(X) * modInverse(3, 11)),
+    modeEleven(modeEleven(Y) * modInverse(3, 11)),
   ];
 };
 
-const encrypt = ([x1, y1]) => {
-  return xFunction([x1, y1], [9, 4]);
-};
+const encrypt = ([x1, y1]) => xFunction([x1, y1], [9, 4]);
 
-const decrypt = ([x1, y1]) => {
-  return xFunction([x1, y1], [9, 7]);
-};
+const decrypt = ([x1, y1]) => xFunction([x1, y1], [9, 7]);
 
 const TEXT_TO_BE_ENCODED = 'HELLO';
 
 const TEXT_IN_ASCII = [...TEXT_TO_BE_ENCODED]
   .map(char => char.charCodeAt(0))
-  .reduce((previous, current) => previous.concat([current.toString().split('')]), []);
+  .reduce((previous, current) => previous.concat([split(current.toString())]), []);
 
 const ENCRYPTED_TEXT = TEXT_IN_ASCII.map(encrypt);
 
